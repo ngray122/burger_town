bc78f02d3eeeab92af0ed4626d88076334ad1840
Nicole Gray <ngray122@gmail.com>
2022-05-13 11:58:26 -0400

    render api item successful

--- client/src/components/SearchCategories.jsx
+++ client/src/components/SearchCategories.jsx
@@ -9,15 +9,21 @@ const SearchCategories = () => {
       // .get(`https://swapi.dev/api/${category}/${id}`)
       .get(`https://bobsburgers-api.herokuapp.com/episodes/`)
       .then((response) => {
-        // console.log("response =>", response);
-        // setApiList(response.data);
-        console.log(response.data);
+        console.log("response =>", response);
+        setApiList(response.data);
+        // console.log(response.data);
       })
       .catch((err) => {
         console.log("error!! ====> ", err);
       });
   }, []);
-  return <div>Home</div>;
+  return (
+    <div>
+      {apiList.map((item, i) => {
+        return <h1>{item.name}</h1>;
+      })}
+    </div>
+  );
 };
 
 export default SearchCategories;
