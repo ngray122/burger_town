0530beeb6d98d3a97fc8995e5f4a16a46db10722
Nicole Gray <ngray122@gmail.com>
2022-05-19 14:50:31 -0400

    forgot to switch my branch

--- client/src/components/card/Category.jsx
+++ client/src/components/card/Category.jsx
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router";
+import {
+  Row,
+  Col,
+  Container,
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+} from "reactstrap";
 
 const Category = () => {
   const [categoryList, setCategoryList] = useState({});
   const headers = Object.values(categoryList);
   const { header } = useParams();
+  const columnsPerRow = 4;
 
   useEffect(() => {
     axios
@@ -17,20 +28,31 @@ const Category = () => {
         console.log("error!! ====> ", err);
       });
   }, [header]);
-  return (
-    <div>
-      <h1>{header}</h1>
 
-      <div>
-        {headers.map((header, id) => (
-          <>
-            <h1 key={id}>
-              {header.id} {header.name}
-            </h1>
-          </>
-        ))}
-      </div>
-    </div>
+  const getColumnsForRow = () => {
+    let items = headers.map((header, index) => {
+      return (
+        <Col>
+          <Card key={header}>
+            <CardBody>
+              <CardTitle>Name: {header.name}</CardTitle>
+              <CardText>Voiced By: {header.voicedBy}</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      );
+    });
+    return items;
+  };
+  return (
+    <>
+      <Container>
+        {" "}
+        <Row xs={1} md={columnsPerRow}>
+          {getColumnsForRow()}
+        </Row>
+      </Container>
+    </>
   );
 };
 
