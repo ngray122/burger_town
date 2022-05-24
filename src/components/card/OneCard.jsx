0df02b9ba78ca701fb6b8fea099c4c68e2cad9f8
Nicole Gray <ngray122@gmail.com>
2022-05-24 10:41:25 -0400

    implicit return

--- src/components/card/OneCard.jsx
+++ src/components/card/OneCard.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { Card } from "reactstrap";
 
-const OneCard = (props) => {
-  return (
-    <div>
-      <Card>{props.children}</Card>
-    </div>
-  );
-};
+const OneCard = (props) => (
+  <div>
+    <Card>{props.children}</Card>
+  </div>
+);
 export default OneCard;
