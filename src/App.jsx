eb2ef00ec03d61339ccc10c9d3081b443e047c66
Nicole Gray <ngray122@gmail.com>
2022-06-14 16:38:40 -0400

    rm unused code from App and deleted SearchCard

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,12 @@ import "./App.css";
 import Landing from "./components/landing/Landing";
 import NavBarComponent from "./components/nav/NavBarComponent";
 import { Routes, Route } from "react-router";
-import { useParams } from "react-router-dom";
 import Category from "./components/card/Category";
-import SearchCard from "./components/search/SearchCard";
 import ApiProvider from "./components/context/ApiContext";
 import { BrowserRouter } from "react-router-dom";
 import { useState } from "react";
 export const ApiContext = React.createContext("");
 function App() {
-  // const { singleHeader } = useParams({});
   const [singleHeader, setSingleHeader] = useState([]);
   const [allHeaders, setAllHeaders] = useState([]);
   return (
