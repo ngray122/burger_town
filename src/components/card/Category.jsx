5fcb56fe3fb9449e58dd49d3574eb178d5523854
jaygraygray <jeremygray001@gmail.com>
2022-06-18 12:54:58 -0400

    chore: comment about de-duping context values

--- src/components/card/Category.jsx
+++ src/components/card/Category.jsx
@@ -5,6 +5,10 @@ import SearchBar from "../search/SearchBar";
 import { ApiContext } from "../../App";
 
 const Category = () => {
+  // you're passing a lot of these as props to ItemPagination,
+  // but you're also using the ApiContext there ...
+  // there's no need to pass them as props since
+  // you already have access to them in ItemPagination
   const {
     setCurrentPage,
     allHeaders,
@@ -14,8 +18,12 @@ const Category = () => {
     setPath,
     path,
   } = useContext(ApiContext);
+  // same with this function, you can define this in ItemPagination
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
   useEffect(() => {
+    // if you aren't returning something in a .map
+    // then use forEach
+    // .map is for creating new arrays
     headers.map((header) => {
       setPath(header);
     });
