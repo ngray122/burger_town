7c6bcb0ee528071b5e453d15392f7c1abecbbf92
Nicole Gray <ngray122@gmail.com>
2022-06-21 14:40:32 -0400

    onclick one modal woring again

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -29,7 +29,23 @@ const SearchBar = () => {
 
   // move .map logic in getQueryMatch, and use
   // allItems
-  const renderQueryMatch = () => {};
+  const renderQueryMatch = () => {
+    // getQueryMatch();
+    // allItems.map((item) => {
+    //   return (
+    //     <Col key={item.id} onClick={() => handleToggle(item)}>
+    //       <OneCard>
+    //         <CardBody>
+    //           <CardImg image={item.image} />
+    //           <CardTitle name={item.name} />
+    //           <CardSubtitle season={item.season} episode={item.episode} />
+    //           <CardSubtitle price={item.price} />
+    //         </CardBody>
+    //       </OneCard>
+    //     </Col>
+    //   );
+    // });
+  };
 
   const handleToggle = useCallback(
     (item) => {
@@ -65,7 +81,6 @@ const SearchBar = () => {
       });
 
     // these items need to live in local state
-    // setAllItems(items.slice(indexOfFirstPost, indexOfLastPost))
     return items.slice(indexOfFirstPost, indexOfLastPost);
   };
   const indexOfLastPost = currentPage * itemsPerPage;
