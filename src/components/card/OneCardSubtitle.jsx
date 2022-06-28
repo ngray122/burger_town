a4752723a45b1957eeddb709b7ad4ad6a7f3866d
Nicole Gray <ngray122@gmail.com>
2022-06-28 16:47:27 -0400

    transform on hover

--- src/components/card/OneCardSubtitle.jsx
+++ src/components/card/OneCardSubtitle.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import CardText, { CardSubtitle } from "reactstrap";
+import styles from "./OneCard.module.css";
 
 const OneCardSubtitle = ({ firstEpisode }) =>
   firstEpisode ? (
-    <CardSubtitle tag="h6">First Episode: {firstEpisode}</CardSubtitle>
+    <CardSubtitle className={styles.card_subtitle} tag="h6">
+      First Episode: {firstEpisode}
+    </CardSubtitle>
   ) : null;
 export default OneCardSubtitle;
